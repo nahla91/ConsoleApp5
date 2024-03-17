3698a364d449428592d9a92a108221d409ff05a7
Nahla <Nahla@NAHLA>
2024-03-17 12:41:21 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static int DeciemalToOCTAL (int d)
+        {
+            if (d == 0)
+            {
+                return 0;
+
+            }
+            else 
+                return(d%2+10*DeciemalToOCTAL (d/2));
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int d = 22;
+            Console.WriteLine(DeciemalToOCTAL(d));
+            Console.ReadKey();
+        }
+    }
+}
